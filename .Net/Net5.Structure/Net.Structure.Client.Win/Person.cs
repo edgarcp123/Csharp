79ca3a48e26cf9d63418c2ecf58985a04e5d2340
Edgar Castillo <edgarcp123@gmail.com>
2021-02-17 11:23:18 -0500

    primer commit

---
+++ .Net/Net5.Structure/Net.Structure.Client.Win/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net.Structure.Client.Win.Model
+{
+    public class Person
+    {
+        //propiedad es publico
+        //atributo es privado
+
+        /*
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        */
+
+        public string name { get; set; }
+    }
+}
